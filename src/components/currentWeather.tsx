82481d0581bb94434f6f72a08662c79cf6845ee3
Maxime Lafitte <maxime.lafitte@gmail.com>
2021-08-02 16:11:08 +0200

    add responsive

--- src/components/currentWeather.tsx
+++ src/components/currentWeather.tsx
@@ -8,18 +8,21 @@ import { CurrentContainer,
          CurrentWeatherInfo, 
          Separation 
         } from "../styles/currentWeather";
+import { Paragraph, Loading } from "../styles/globalStyle";
 import Moment from 'react-moment';
-import moment from 'moment-with-locales-es6';
+import moment from 'moment';
 
 const CurrentWeather = () => {
 
     const [current, setCurrent] = useState<CurrentWeatherProps | null>(null)
+    const [loading, setLoading] = useState(true)
 
     const fetchWeather = () => {
         axios.get<CurrentWeatherProps>(`https://api.openweathermap.org/data/2.5/weather?q=paris&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
         .then(res => {
         setCurrent(res.data)
-        console.log(res.data)
+        setLoading(false)
+        // console.log(res.data)
         })
         .catch(function (err) {
             console.log(err)
@@ -34,18 +37,25 @@ const CurrentWeather = () => {
         <div>
             <CurrentContainer>
                 {
+                    loading && (
+                        <Loading>
+                            Loading ...
+                        </Loading>
+                    )
+                }
+                {
                     current && (
                         <>
                             <CurrentWeatherInfo>
                                 <>
                                     <CurrentWeatherTitle>{current.name}</CurrentWeatherTitle>
-                                    <Moment date={moment()} format="dddd Do MMM" />
+                                    <Moment date={moment()} format="dddd Do MMMM" />
                                     <Separation/>
                                     <CurrentWeatherTitle>{current.main.temp.toFixed(0)} C°</CurrentWeatherTitle>
-                                    <span>{current.main.temp_min.toFixed(0)} C° / {current.main.temp_max.toFixed(0)} C°</span>
+                                    <span>{current.main.temp_max.toFixed(0)} C°/ {current.main.temp_min.toFixed(0)} C°</span>
                                     <CurrentWeatherDescription>
                                         <img src={`http://openweathermap.org/img/wn/${current.weather[0].icon}.png`} alt={current.weather[0].description} />
-                                        <span>{current.weather[0].description}</span>
+                                        <Paragraph>{current.weather[0].description}</Paragraph>
                                     </CurrentWeatherDescription>
                                 </>
                             </CurrentWeatherInfo>
--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,10 +1,18 @@
 import styled from "styled-components"
+import { device } from './devices'
 
 export const Container = styled.div`
   display: block;
-  margin: 5% 30%;
+  margin: 5% 20%;
+  box-shadow: 5px 6px 18px 0px rgba(0,0,0,0.22);
+  @media ${device.laptop}  { 
+    margin: 0;
+  }
 `
-
 export const Paragraph = styled.div`
 	margin: 3px 0px;
+  text-transform: capitalize
+`
+export const Loading = styled.div`
+  text-align: center;
 `
